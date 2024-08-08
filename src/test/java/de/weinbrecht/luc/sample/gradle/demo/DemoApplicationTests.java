191880f561260c3924b65efd6d6fad218dc31ba4
Luc Weinbrecht <luc@weinbrecht.de>
2024-08-08 17:15:40 +0200

    initial commit

---
+++ src/test/java/de/weinbrecht/luc/sample/gradle/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.weinbrecht.luc.sample.gradle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
